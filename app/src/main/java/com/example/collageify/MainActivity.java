812fa572eb1f068fe771d789f5a4ed8845daccb1
jasmine-y-wang <jasminewang@fb.com>
2022-06-20 16:20:28 -0700

    authenticate with spotify-auth

--- app/src/main/java/com/example/collageify/MainActivity.java
+++ app/src/main/java/com/example/collageify/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.collageify;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -12,12 +14,16 @@ import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
+import com.spotify.sdk.android.auth.AuthorizationClient;
+import com.spotify.sdk.android.auth.AuthorizationRequest;
+import com.spotify.sdk.android.auth.AuthorizationResponse;
 
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String CLIENT_ID = "241d0f56be9e4824b0ededa9f2c4ae3d";
-    public static final String REDIRECT_URI = "http://com.example.collageify/callback";
+    public static final String REDIRECT_URI = "com.example.collageify://callback";
+    public static final int REQUEST_CODE = 1337;
     private SpotifyAppRemote mSpotifyAppRemote;
     public static final String TAG = "MainActivity";
 
@@ -25,33 +31,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        authenticateSpotify();
+    }
+
+    private void authenticateSpotify() {
+        AuthorizationRequest.Builder builder = new AuthorizationRequest.Builder(CLIENT_ID,
+                AuthorizationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{"streaming"});
+        AuthorizationRequest request = builder.build();
+        AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        ConnectionParams connectionParams = new ConnectionParams.Builder(CLIENT_ID)
-                .setRedirectUri(REDIRECT_URI)
-                .showAuthView(true)
-                .build();
-        SpotifyAppRemote.connect(this, connectionParams, new Connector.ConnectionListener() {
-            @Override
-            public void onConnected(SpotifyAppRemote spotifyAppRemote) {
-                mSpotifyAppRemote = spotifyAppRemote;
-                Log.d(TAG, "connected! yay!");
-                connected();
-            }
-
-            @Override
-            public void onFailure(Throwable error) {
-                Log.e("MainActivity", error.getMessage(), error);
-            }
-        });
     }
 
-    private void connected() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            AuthorizationResponse response = AuthorizationClient.getResponse(resultCode, data);
 
+            switch (response.getType()) {
+                case TOKEN:
+                    // handle successful response
+                    Log.d(TAG, "connected! yay!");
+                    break;
+
+                case ERROR:
+                    // handle error response
+                    Log.e(TAG, "there was an error logging in");
+                    break;
+
+                default:
+                    // handle other cases
+            }
+
+        }
     }
+
 
     @Override
     protected void onStop() {
