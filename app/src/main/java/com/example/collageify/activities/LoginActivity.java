3c8441388c4684f70736b52ac8df38722e07aba1
jasmine-y-wang <jasminewang@fb.com>
2022-06-20 22:42:27 -0700

    working on spotify auth

--- app/src/main/java/com/example/collageify/activities/LoginActivity.java
+++ app/src/main/java/com/example/collageify/activities/LoginActivity.java
@@ -1,14 +1,18 @@
-package com.example.collageify;
+package com.example.collageify.activities;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.example.collageify.R;
 import com.spotify.sdk.android.auth.AuthorizationClient;
 import com.spotify.sdk.android.auth.AuthorizationRequest;
 import com.spotify.sdk.android.auth.AuthorizationResponse;
@@ -20,12 +24,20 @@ public class LoginActivity extends AppCompatActivity {
     public static final int REQUEST_CODE = 1337;
     public static final String SCOPES = "user-read-recently-played,user-top-tracks,user-read-private";
     public static final String TAG = "LoginActivity";
+    private SharedPreferences.Editor editor;
+    private SharedPreferences mSharedPreferences;
+    private RequestQueue queue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Button btnLoginSpotify = findViewById(R.id.btnLoginSpotify);
+
+        mSharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+        queue = Volley.newRequestQueue(this);
+
         btnLoginSpotify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,12 +63,16 @@ public class LoginActivity extends AppCompatActivity {
             switch (response.getType()) {
                 case TOKEN:
                     // handle successful response
-                    Log.d(TAG, "connected! yay!");
+                    editor = getSharedPreferences("SPOTIFY", 0).edit();
+                    editor.putString("token", response.getAccessToken());
+                    Log.d(TAG, "starting: got auth token");
+                    editor.apply();
+//                    waitForUserInfo();
                     break;
 
                 case ERROR:
                     // handle error response
-                    Log.e(TAG, "there was an error logging in");
+                    Log.e(TAG, "there was an error logging in: " + response.getError());
                     break;
 
                 default:
@@ -64,6 +80,9 @@ public class LoginActivity extends AppCompatActivity {
             }
 
         }
+    }
+
+    private void waitForUserInfo() {
     }
 
 
---
+++ app/src/main/java/com/example/collageify/activities/SignupActivity.java
@@ -0,0 +1,33 @@
+package com.example.collageify.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.collageify.R;
+
+
+public class SignupActivity extends AppCompatActivity {
+
+
+    public static final String TAG = "SignupActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
